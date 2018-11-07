a503b2d44288b4504e92fa4da4ca42d48e912962
José Carlos <josecarlos.n.f99@gmail.com>
2018-11-07 16:04:45 -0200

    Questão 1172 do Uri com com função

---
+++ 1172_def.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 1172 - Substituição em Vetor I
+# Função e Procedimento
+
+def função(entrada):
+	if entrada <= 0:
+		saida = 1
+	else:
+		saida = entrada
+	return saida
+
+
+
+# Main
+lista = list()
+
+for i in range(10):
+	entrada = int(input())
+	lista.append(função(entrada))
+	print('X[{}] = {}'.format(i, lista[i]))
