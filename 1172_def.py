d1ca04496b9ce21666c41d4ed13c47df4289bfae
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2018-11-07 20:57:56 -0300

    1172 formatação PEP8

--- 1172_def.py
+++ 1172_def.py
@@ -2,19 +2,19 @@
 # 1172 - Substituição em Vetor I
 # Função e Procedimento
 
-def função(entrada):
-	if entrada <= 0:
-		saida = 1
-	else:
-		saida = entrada
-	return saida
 
+def função(entrada):
+    if entrada <= 0:
+        saida = 1
+    else:
+        saida = entrada
+    return saida
 
 
 # Main
 lista = list()
 
 for i in range(10):
-	entrada = int(input())
-	lista.append(função(entrada))
-	print('X[{}] = {}'.format(i, lista[i]))
+    entrada = int(input())
+    lista.append(função(entrada))
+    print('X[{}] = {}'.format(i, lista[i]))
